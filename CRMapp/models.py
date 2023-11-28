15bf2d5a719254dc811651ec459e9ff27503b2fa
Hamza Ekhwan <hamzaekhwan2000@gmail.com>
2023-11-28 19:12:05 +0300

    sixth commit

--- CRMapp/models.py
+++ CRMapp/models.py
@@ -22,7 +22,7 @@ class Client(models.Model):
     arabic_name=models.CharField("Arabic Name of Client", max_length=64)
     city=models.CharField("city", max_length=64)
     
-    date=models.DateTimeField("Date of client register",blank=True,null=True)
+    date=models.CharField("Date of client register",max_length=10,blank=True)
     
     def __str__(self):
         return str(self.name ) 
@@ -40,7 +40,7 @@ class Reminder(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     admin= models.ForeignKey(User, on_delete=models.CASCADE)
     message = models.CharField(max_length=255)
-    reminder_datetime = models.DateTimeField()
+    reminder_datetime = models.CharField("Date of reminder_datetime",max_length=10)
     notification_sent = models.BooleanField(default=False)  
 
    
@@ -60,16 +60,16 @@ class Contract(models.Model):
 class MaintenanceLift(models.Model): 
     contract = models.OneToOneField(Contract, unique=True, on_delete=models.CASCADE, related_name='maintenancelift')
     maintenance_contract_number=models.CharField("Maintenance Contract Number", max_length=64)
-    maintenance_contract_start_date=models.DateTimeField("Maintainance Contract Start")
-    maintenance_contract_end_date=models.DateTimeField("Maintainance Contract End")
+    maintenance_contract_start_date=models.CharField("Maintainance Contract Start",max_length=10)
+    maintenance_contract_end_date=models.CharField("Maintainance Contract End",max_length=10)
     maintenance_type = models.CharField("Maintenance Type",blank=True,choices=MAINTAINCANCE_CHOICES, max_length=255)
     contract_value=models.IntegerField()
     spare_parts= models.CharField("Spare parts",choices=SPARE_PARTS, max_length=255)
     brand=models.CharField("Brand", max_length=64)
     number_of_visits_per_year=models.IntegerField()
     villa_no=models.CharField("Villa Number", max_length=64)
-    handing_over_date=models.DateTimeField("Handing Over Date")
-    free_maintenance_expiry_date=models.DateTimeField("free maintenance expiry date")
+    handing_over_date=models.CharField("Handing Over Date",max_length=10)
+    free_maintenance_expiry_date=models.CharField("free maintenance expiry date",max_length=10)
 
 
     def __str__(self):
@@ -80,8 +80,8 @@ class Phase(models.Model):
     contract=models.ForeignKey(Contract,unique=False , on_delete=models.CASCADE)
     Name=models.CharField("Name of Phase",choices=PHASES_NAME, max_length=64)
     isActive=models.BooleanField(default=False)
-    start_date=models.DateTimeField("Phase Date Start")
-    end_date=models.DateTimeField("Phase Date End",blank=True,null=True)
+    start_date=models.CharField("Phase Date Start",max_length=10)
+    end_date=models.CharField("Phase Date End",max_length=10,blank=True,null=True)
     
     def __str__(self):
         return str(self.contract) + " " + str(self.Name ) 
@@ -92,7 +92,7 @@ class Note(models.Model)    :
     contract=models.ForeignKey(Contract,unique=False , on_delete=models.CASCADE)
     note=models.TextField("Notes",blank=True)
     attachment=models.FileField(blank=True)
-    date=models.DateTimeField("Date of note")
+    date=models.CharField("Date of note",max_length=10)
     
     def __str__(self):
         return str(self.contract) + " " + str(self.date)
@@ -106,7 +106,7 @@ class Maintenance(models.Model):
     type_name= models.CharField("Type of Maintenance ",choices=MAINTENANCETYPE_CHOICES, max_length=255)
     remarks=models.TextField()
     check_image=models.FileField(blank=True) 
-    date=models.DateTimeField("Date of remark")
+    date=models.CharField("Date of remark",max_length=10)
 
     def __str__(self):
         return str(self.contract) + " " + str(self.type_name ) 
